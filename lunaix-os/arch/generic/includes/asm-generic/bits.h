3d33d9c0d1894c447324d8327200c212ddad3716
Lunaixsky <lunaixsky@qq.com>
2025-01-14 13:36:35 +0000

    move syscall_nr to generic for sharing between arch impls
    fix the include error of arch-listing. move them to generic as fallback
    add default pool declaration to pmm_arch_init_pool

--- lunaix-os/arch/generic/includes/asm-generic/bits.h
+++ lunaix-os/arch/generic/includes/asm-generic/bits.h
@@ -2,12 +2,15 @@
 #define __LUNAIX_ARCH_GENERIC_BITS_H
 
 #define _BITS_GENMASK(h, l)     \
-        (((1UL << ((h) + 1)) - 1) ^ ((1UL << (l)) - 1))
+        (((1 << ((h) + 1)) - 1) ^ ((1 << (l)) - 1))
 
 #define _BITS_EXTRACT(from, h, l)     \
-        (((from) & (((1UL << (h + 1)) - 1) ^ ((1UL << l) - 1))) >> l)
+        (((from) & (((1 << (h + 1)) - 1) ^ ((1 << (l)) - 1))) >> (l))
 
 #define _BITS_INSERT(to, from, h, l)    \
-        (((to) & ~_BITS_GENMASK(h, l)) | (((from) << l) & _BITS_GENMASK(h, l)))
+        (((to) & ~_BITS_GENMASK(h, l)) | (((from) << (l)) & _BITS_GENMASK(h, l)))
+
+#define _BITS_STATIC(val, h, l)         \
+        (((val) & _BITS_GENMASK(h, l)) << (l))
 
 #endif /* __LUNAIX_ARCH_BITS_H */
--- lunaix-os/arch/generic/vmutils.c
+++ lunaix-os/arch/generic/vmutils.c
@@ -10,11 +10,30 @@ dup_leaflet(struct leaflet* leaflet)
 _default void
 pmm_arch_init_pool(struct pmem* memory)
 {
-    fail("unimplemented");
+    pmm_declare_pool(POOL_UNIFIED, 1, memory->list_len);
 }
 
 _default ptr_t
 pmm_arch_init_remap(struct pmem* memory, struct boot_handoff* bctx)
 {
     fail("unimplemented");
+}
+
+_default pte_t
+translate_vmr_prot(unsigned int vmr_prot, pte_t pte)
+{
+    pte = pte_mkuser(pte);
+
+    if ((vmr_prot & PROT_WRITE)) {
+        pte = pte_mkwritable(pte);
+    }
+
+    if ((vmr_prot & PROT_EXEC)) {
+        pte = pte_mkexec(pte);
+    }
+    else {
+        pte = pte_mknexec(pte);
+    }
+
+    return pte;
 }
